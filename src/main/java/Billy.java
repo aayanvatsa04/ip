86ee14b227de9f555d65422042299edd94c5bca6
Aayan Vatsa <aayanvatsa@gmail.com>
2026-08-21 02:42:22 +0800

    Rename chatbot to Billy

---
+++ src/main/java/Billy.java
@@ -0,0 +1,11 @@
+public class Billy {
+    public static void main(String[] args) {
+        String banner = " ____  _ _ _       \n"
+                + "| __ )(_) | |_   _ \n"
+                + "|  _ \\| | | | | | |\n"
+                + "| |_) | | | | |_| |\n"
+                + "|____/|_|_|_|\\__, |\n"
+                + "             |___/ \n";
+        System.out.println(banner);
+    }
+}
